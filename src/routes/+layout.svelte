63548194ddaf60804d628837fe4176a1f4879b6d
Sam Cheng <51756184+Samalander0@users.noreply.github.com>
2024-01-05 17:13:58 -0800

    Added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
+<script>
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  
+  inject({ mode: dev ? 'development' : 'production' });
+</script>
+
 <div id="smooth-wrapper">
   <div id="smooth-content">
     <slot></slot>
